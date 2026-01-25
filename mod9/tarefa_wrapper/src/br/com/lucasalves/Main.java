841caa71b3e540971633f4b27b413781c09d8d47
Lucas Alves <lucasfalves2030@gmail.com>
2026-01-25 00:05:17 -0300

    tarefa do módulo 9 referente a wrappers

---
+++ mod9/tarefa_wrapper/src/br/com/lucasalves/Main.java
@@ -0,0 +1,14 @@
+package br.com.lucasalves;
+
+public class Main {
+
+    public static void main (String args[]){
+
+        int numero = 10;
+        System.out.println(numero);
+
+        Integer numeroWrapper = numero;
+        System.out.print(numeroWrapper);
+
+    }
+}
